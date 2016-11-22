5d5b16986d8adb463e2cbca0e14018a73477d009
racine-p-a <5IuAYjqv&>
2016-11-22 09:17:48 +0100

    Fin de l'extraction vers javascript.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args)
+    {
+        //System.out.println("Hello World!");
+        String resultatJavaScript ="";
+        Document doc = null;
+        try
+        {
+            doc = Jsoup.connect("https://fr.wikipedia.org/wiki/Liste_de_langues").get();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        //String title = doc.title();
+        //System.out.println(title);
+        Elements resultLinks = doc.select("li > a");
+        for (Element element : resultLinks )
+        {
+            if(element.text().length() > 1)
+            {
+                resultatJavaScript += "\"" + element.text() + "\": " + "\"" + element.text().substring(0, 1).toUpperCase()+ element.text().substring(1).toLowerCase() + "\",\n";
+            }
+            //System.out.println(element.text());
+
+        }
+        //System.out.println(resultLinks.getClass());
+        //String linkText = resultLinks.text();
+        //System.out.println(linkText.getClass());
+        System.out.print(resultatJavaScript);
+
+
     }
 }
